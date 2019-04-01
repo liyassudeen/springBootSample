237635a7cc005daa27356f80c274ec0f3045324e
MohamedLiyassudeen <mohamed.m@skava.com>
2019-04-01 18:39:42 +0530

    Get the dynamic param using @PathVariable

--- src/main/java/sample/demo/web/DemoController.java
+++ src/main/java/sample/demo/web/DemoController.java
@@ -2,13 +2,14 @@ package sample.demo.web;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class DemoController {
 
-    @GetMapping("/demo")
-    public String index() {
-        return "Hi I'm demo";
+    @GetMapping("/demo/{id}")
+    public String index(@PathVariable String id) {
+        return "Hi I'm demo with "+id+"";
     }
 
 }
